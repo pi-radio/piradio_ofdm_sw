83d26583678ba1279bedee05ff4af20829874e1b
George Vardakis <vardakis@libre.space>
2023-07-05 13:36:53 +0200

    Add ofdm setup files

---
+++ ofdm_setup/piradio_setup.sh
@@ -0,0 +1,10 @@
+insmod files/piradio0.ko &&
+ip link set piradio00 mtu 8000 &&
+ip link set piradio01 mtu 8000 &&
+ip link set piradio00 up &&
+ip link set piradio01 up &&
+./zcu111_setup.elf -r 2 -t sync_temp.bin -m map.bin -p &&
+ip addr add 192.168.4.4/24 dev piradio00 &&
+ip addr add 192.168.5.5/24 dev piradio01 &&
+devmem2 0xB0010000 h 3 &&
+devmem2 0xB0011000 h 3
---
+++ ofdm_setup/remove.sh
@@ -0,0 +1,3 @@
+ip link set piradio00 down &&
+ip link set piradio01 down &&
+rmmod piradio0
